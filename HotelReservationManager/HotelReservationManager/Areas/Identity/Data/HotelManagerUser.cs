441a96c338f13a55e1ce9f45c63dfe4840535363
Samuil <35292702+sami119@users.noreply.github.com>
2020-02-26 15:57:40 +0200

    Initial Identity
    - Initial Create
    - Custom User Account
    - Aded User Roles
    -- Admin
    -- Client
    - Started working on Admin Panel

---
+++ HotelReservationManager/HotelReservationManager/Areas/Identity/Data/HotelManagerUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelReservationManager.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the HotelManagerUser class
+    public class HotelManagerUser : IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+
+        [PersonalData]
+        public string Surname { get; set; }
+
+        [PersonalData]
+        public string Familyname { get; set; }
+
+        [PersonalData]
+        public string EGN { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [PersonalData]
+        public DateTime DateOfAppointment { get; set; }
+
+        [PersonalData]
+        public DateTime DateOfDismissal { get; set; }
+    }
+}
