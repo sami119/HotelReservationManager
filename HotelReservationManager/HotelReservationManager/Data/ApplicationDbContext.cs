863f06a7ed858caa012dddb615cacc854809c5ae
Hristo Kovachev <hristo.kovachev@zigzag.global>
2020-02-26 23:10:41 +0200

    Adding Clients MVC, DB and visuals updates

--- HotelReservationManager/HotelReservationManager/Data/ApplicationDbContext.cs
+++ HotelReservationManager/HotelReservationManager/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using HotelReservationManager.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,11 @@ namespace HotelReservationManager.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Client> Clients
+        {
+            get; set;
         }
     }
 }
