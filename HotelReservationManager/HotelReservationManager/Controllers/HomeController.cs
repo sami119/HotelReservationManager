fd35af9ef06ff1aa16c85d2a1b6726d22d3fd1da
Samuil <sami.slavchev@gmail.com>
2020-02-26 18:41:32 +0200

    Fix and Service
    Added Service
    - GetCurrentUserId
    
    Fixed redirection to the registred account
    
    Co-Authored-By: onlybadnews <onlybadnews@users.noreply.github.com>

--- HotelReservationManager/HotelReservationManager/Controllers/HomeController.cs
+++ HotelReservationManager/HotelReservationManager/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HotelReservationManager.Models;
+using HotelReservationManager.Services;
 
 namespace HotelReservationManager.Controllers
 {
---
+++ HotelReservationManager/HotelReservationManager/Services/UserIdentityProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HotelReservationManager.Services
+{
+    public class UserIdentityProvider : IUserIdentityProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserIdentityProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Вижда в базата данни кой е потребителя и връща ид-то му
+        /// </summary>
+        /// <returns>User.Id -> string format</returns>
+        public virtual string GetCurrentUserId()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return userId;
+        }
+    }
+}
